427e4baba79eadb6f8e5e9eb56d6f6c0987a8344
Artem Tseranu <artbqts@gmail.com>
2017-06-25 12:08:06 +0300

    Improve 'Create plan' mutation
    Now calendarType and calendarDate input fields are not required.

--- app/graph/types/create_plan_mutation_type.rb
+++ app/graph/types/create_plan_mutation_type.rb
@@ -1,3 +1,5 @@
+require "constants"
+
 module Types
   CreatePlanMutationType = GraphQL::Relay::Mutation.define do
     name "CreatePlan"
@@ -5,15 +7,33 @@ module Types
     input_field :name, !types.String
     input_field :start, !types.String
     input_field :finish, !types.String
-    input_field :calendarType, !types.String
-    input_field :calendarDate, !types.String
+    input_field :calendarType, types.String
+    input_field :calendarDate, types.String
 
     return_field :newPlan, PlanType
     return_field :calendar, CalendarType
 
     resolve -> (object, inputs, context) do
-      new_plan = Plan.create!(user: User.first, name: inputs[:name], start: inputs[:start], finish: inputs[:finish])
-      calendar = Calendar.for(inputs[:calendarType], Time.parse(inputs[:calendarDate]))
+      new_plan =
+        Plan.create!(
+          user: User.first,
+          name: inputs[:name],
+          start: Time.parse(inputs[:start]).utc,
+          finish: Time.parse(inputs[:finish]).utc
+        )
+
+      calendar_date =
+        if inputs[:calendarDate].present?
+          Time.parse(inputs[:calendarDate])
+        else
+          Time.now.utc
+        end
+
+      calendar =
+        Calendar.for(
+          inputs[:calendarType] || Constants::DAY_CALENDAR_TYPE_ID,
+          calendar_date
+        )
 
       {newPlan: new_plan, calendar: calendar}
     end
